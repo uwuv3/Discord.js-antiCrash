1898e69722c9b1b8330c30fc968f1d84b86466b4
uwuv3 <100004271+uwuv3@users.noreply.github.com>
2022-05-27 16:32:36 +0300

    Create index.js

---
+++ v13/index.js
@@ -0,0 +1,10 @@
+const { MessageEmbed } = require("discord.js") //v13 -_-
+const chalk = require("chalk"); //@3.0.0
+//client 
+/* example :
+const client = new Client({intents: ["GUILDS","GUILD_MEMBERS","GUILD_BANS","GUILD_EMOJIS_AND_STICKERS","GUILD_INTEGRATIONS","GUILD_WEBHOOKS","GUILD_INVITES","GUILD_VOICE_STATES","GUILD_PRESENCES","GUILD_MESSAGES","GUILD_MESSAGE_REACTIONS","GUILD_MESSAGE_TYPING","DIRECT_MESSAGES","DIRECT_MESSAGE_REACTIONS","DIRECT_MESSAGE_TYPING","GUILD_SCHEDULED_EVENTS"],
+  partials: ["MESSAGE", "CHANNEL", "REACTION"],
+});
+all intents 
+*/
+require("./antiCrash.js")(client,MessageEmbed,chalk);
