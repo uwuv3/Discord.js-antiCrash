0e958acf2259f7ad566ddd24ad52d136258cc049
uwuv3 <100004271+uwuv3@users.noreply.github.com>
2022-05-27 16:36:12 +0300

    Update index.js

--- v13/index.js
+++ v13/index.js
@@ -1,5 +1,6 @@
 const { MessageEmbed } = require("discord.js") //v13 -_-
 const chalk = require("chalk"); //@3.0.0
+const config = require("./config.json")
 //client 
 /* example :
 const client = new Client({intents: ["GUILDS","GUILD_MEMBERS","GUILD_BANS","GUILD_EMOJIS_AND_STICKERS","GUILD_INTEGRATIONS","GUILD_WEBHOOKS","GUILD_INVITES","GUILD_VOICE_STATES","GUILD_PRESENCES","GUILD_MESSAGES","GUILD_MESSAGE_REACTIONS","GUILD_MESSAGE_TYPING","DIRECT_MESSAGES","DIRECT_MESSAGE_REACTIONS","DIRECT_MESSAGE_TYPING","GUILD_SCHEDULED_EVENTS"],
@@ -7,4 +8,4 @@ const client = new Client({intents: ["GUILDS","GUILD_MEMBERS","GUILD_BANS","GUIL
 });
 all intents 
 */
-require("./antiCrash.js")(client,MessageEmbed,chalk);
+require("./antiCrash.js")(client,MessageEmbed,chalk,config);
